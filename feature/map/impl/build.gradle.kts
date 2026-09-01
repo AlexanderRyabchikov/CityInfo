604a2d033d5f02b5c8096bccef3df863e16dea54
Alexander Ryabchikov <alexasp3@gmail.com>
2026-09-01 12:41:52 +0300

    fix: read Yandex MapKit key from config

--- feature/map/impl/build.gradle.kts
+++ feature/map/impl/build.gradle.kts
@@ -1,5 +1,22 @@
+import com.android.build.api.dsl.LibraryExtension
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.setup.feature.ui)
+}
+
+val secrets = Properties().apply {
+    val file = rootProject.file("local.properties")
+    if (file.exists()) file.inputStream().use(::load)
+}
+
+fun secret(name: String): String =
+    secrets.getProperty(name) ?: System.getenv(name).orEmpty()
+
+configure<LibraryExtension> {
+    defaultConfig {
+        buildConfigField("String", "YANDEX_MAP_KEY", "\"${secret("YANDEX_MAP_KEY")}\"")
+    }
 }
 
 dependencies {
